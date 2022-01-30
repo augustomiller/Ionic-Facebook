3da11f2f181a791d363a7185ce9d2d9089da251e
Augusto  Miller <augusto.miller@primecontrol.com.br>
2022-01-30 16:05:56 -0300

    16 Criando SharedModule para recursos compartilhados

---
+++ IonicFirestoneTasks/src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+  ]
+})
+export class SharedModule { }
